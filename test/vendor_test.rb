2239ce9fe718fa59095f00ba4da97aeb3106a5b8
DougWelchons <doug.welchons@gmail.com>
2021-01-20 09:46:39 -0700

    Add additional vendor class tests

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -32,4 +32,26 @@ class VendorTest < Minitest::Test
 
     assert_equal 55, vendor.check_stock(item1)
   end
+
+  def test_it_can_sell_an_item
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'apple', price: "$0.75"})
+
+    vendor.stock(item1, 30)
+
+    assert_equal true, vendor.sell?(item1)
+    assert_equal false, vendor.sell?(item2)
+  end
+
+  def test_it_can_return_potential_revenue
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'apple', price: "$0.50"})
+
+    vendor.stock(item1, 10)
+    vendor.stock(item2, 20)
+
+    assert_equal 17.5, vendor.potential_revenue
+  end
 end
