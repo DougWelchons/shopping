e84799a99b43a9b87724c7f4d200d9da54148241
DougWelchons <doug.welchons@gmail.com>
2021-01-20 10:09:06 -0700

    Add total_inventory method to market class

--- lib/market.rb
+++ lib/market.rb
@@ -21,4 +21,15 @@ class Market
       vendor.sell?(item)
     end
   end
+
+  def total_inventory
+    @vendors.reduce({}) do |total_inventory, vendor|
+      vendor.inventory.each do |item, qty|
+        total_inventory[item] ||= {:quantity => 0, :vendors => []}
+        total_inventory[item][:quantity] += qty
+        total_inventory[item][:vendors] << vendor
+      end
+      total_inventory
+    end
+  end
 end
